b60fd31ebdc8068d917c3882500f56f0a2febf35
Monster737 <73monster37@gmail.com>
2021-06-04 13:53:57 +0300

    Initial commit

---
+++ Practuchne 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        
+        public delegate double SimpleDelegate(double x, double y);
+
+        
+        
+            static void Main(string[] args)
+            {
+            double a = Double.Parse(Console.ReadLine());
+            double b = Double.Parse(Console.ReadLine());
+
+            SimpleDelegate average = GetAverage;
+                Console.WriteLine(average(a, b));
+                Console.ReadLine();
+            }
+
+
+            private static double GetAverage(double x, double y)
+            {
+                return (x + y) / 2;
+            }
+        
+    }
+}
